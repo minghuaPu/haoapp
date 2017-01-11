6dc4384e5c3eef4971eb9c676b014b43342cc5fa
smith <3162547163@qq.com>
2017-01-11 09:19:11 +0800

    职位列表数据更新

--- js/jobseekers/controllers/job.js
+++ js/jobseekers/controllers/job.js
@@ -1,6 +1,6 @@
 angular.module('jobseekers.controllers')
 
-.controller('DashCtrl', function($scope,$ionicModal,$ionicPopover,$state,$ionicViewSwitcher) {
+.controller('DashCtrl', function($scope,$ionicModal,$ionicPopover,$state,$ionicViewSwitcher,Jobs) {
      $ionicModal.fromTemplateUrl('templates/index_search.html', {
         scope: $scope,
          animation: 'animated fadeInRightBig'
@@ -20,6 +20,49 @@ angular.module('jobseekers.controllers')
         scope: $scope
       });
 
+      //解决页面一开始调用数据
+       Jobs.GetFeed().then(function(response){
+
+            Jobs.hasmore = response.data.length>0;
+            $scope.job_lists=response.data;
+          
+         
+        })
+
+      $scope.doRefresh=function  () {
+        if (Jobs.hasmore) {
+          Jobs.GetFeed().then(function  (rtn_data) {
+             Jobs.hasmore = rtn_data.data.length>0;//>0有数据
+             $scope.job_lists=rtn_data.data.concat($scope.job_lists);
+          })
+        }
+          $scope.$broadcast('scroll.refreshComplete');//隐藏下拉刷新
+
+        return;
+      }
+
+      $scope.loadMore=function  () {
+        console.log(Jobs.hasmore);
+        if (Jobs.hasmore) {
+          
+          Jobs.GetFeed().then(function  (rtn_data) {
+             Jobs.hasmore = rtn_data.data.length>0;//>0有数据
+              //数据绑定
+              // $scope.job_lists=
+             for(var i=0;i<rtn_data.data.length;i++){
+                $scope.job_lists.push(rtn_data.data[i]);
+             }
+          })
+        }
+          $scope.$broadcast('scroll.infiniteScrollComplete');//隐藏加载更多
+        
+        return;
+      }
+
+      $scope.moreDataCanBeLoaded = function(){
+         return Jobs.hasmore;
+        }
+
       // .fromTemplateUrl() 方法
       $ionicPopover.fromTemplateUrl('my-popover.html', {
         scope: $scope
@@ -34,6 +77,8 @@ angular.module('jobseekers.controllers')
       $scope.closePopover = function() {
         $scope.popover.hide();
       };
+
+
 
 })
 
