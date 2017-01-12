c0ebb331bb670fd8fa70018097f065a62634c544
smith <3162547163@qq.com>
2017-01-12 14:49:32 +0800

    fix

--- js/config.js
+++ js/config.js
@@ -1,17 +1,5 @@
 angular.module("enterprise.config", [])
-<<<<<<< HEAD
-    .constant("ENV", {
-        // "name": "production",
-        "accessToken": '',
-        "debug": false,
-        "api": "http://localhost/thinkphp_5.0.2_full",
-        "appleId": 'id981408438',
-        'ent_img_path':'img/enterprise',
-        'ent_temp_path':'templates/enterprise',
-        'version':'1.0.1'
-    })
-; 
-=======
+
 	.constant("ENV", {
 		// "name": "production",
 		"accessToken": '',
@@ -22,7 +10,6 @@ angular.module("enterprise.config", [])
 		'ent_temp_path': 'templates/enterprise',
 		'version': '1.0.1'
 	});
->>>>>>> 1260672c2d9486b9a573cf3599f9e3ad23575c30
 angular.module("jobseekers.config", [])
 	.constant("ENV", {
 		// "name": "production",
